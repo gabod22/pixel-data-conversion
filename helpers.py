2b3a3787b7d79d4f652137b58fcb4a962c20beaa
Brian Gabriel Díaz Rodríguez <briangdr.22@gmail.com>
2024-02-05 03:05:39 -0600

    add oc process

--- helpers.py
+++ helpers.py
@@ -1,5 +1,25 @@
 from regex_patterns import pattern_celular, pattern_corchetes
 from constants import arr_no, arr_yes
+import pandas as pd
+
+
+def array_to_string(arr):
+    string = ''
+    for (i, item) in enumerate(arr):
+        if i > 0:
+            string += "\n" + item
+        else:
+            string += item
+    return string
+
+
+def get_last_index(dataframe: pd.DataFrame):
+    """Return the last index of a Pandas Dataframe
+
+    Args:
+        dataframe (pd.DataFrame): Is the dataframe to get the last index
+    """
+    return dataframe[len(dataframe) - 1:].index[0]
 
 
 def clean_testimony(testimony):
@@ -18,7 +38,7 @@ def get_phone_number(strnig):
     match_celular = pattern_celular.search(strnig)
     if match_celular:
         numero_celular = match_celular.group(1)
-        # dispositivo = dispositivo.replace(numero_celular, '')
+        numero_celular = numero_celular.replace(' ', '')
         return numero_celular
     return 'Sin celular'
 
@@ -31,7 +51,6 @@ def get_warranty_status(string, raw: bool):
 
     else:
         garantia = string_split[-1]
-    print(garantia)
     '''Determina si la orden de servicio es garantía y lo añade a un array de elementos
     '''
     if raw and (garantia in arr_no or garantia in arr_yes):
--- index.py
+++ index.py
@@ -2,111 +2,117 @@ import sys
 import pandas as pd
 import re
 from constants import items_cols
-from helpers import get_phone_number, get_warranty_status, clean_testimony
+from helpers import get_phone_number, get_warranty_status, clean_testimony, array_to_string, get_last_index
 
 detailed_service_orders = pd.read_excel(
     'servicios_enero.xlsx')
-# detailed_purchase_orders = pd.read_excel('oc.xlsx')
+detailed_purchase_orders = pd.read_excel('oc.xlsx')
 
 products_workbook_df = pd.read_excel('Productos.xlsx')
 
 
-def procesar_os():
+def procesar_oc():
+    items_oc = {}
+    simplified_purchase_orders = detailed_purchase_orders.dropna(
+        subset=['Folio'], inplace=False)
+    purchase_orders_last_index = get_last_index(detailed_purchase_orders)
+    purchase_order_indexs = simplified_purchase_orders.index.append(
+        pd.Index([purchase_orders_last_index]))
 
+    for inx in range(len(purchase_order_indexs)-1):
+        purchase_orders_items = pd.DataFrame(detailed_purchase_orders.iloc[purchase_order_indexs[inx]+2:purchase_order_indexs[inx+1]-1, [
+            1, 2, 3, 4, 5, 6, 7, 8]])
+        purchase_orders_items.columns = items_cols
+
+        folio = simplified_purchase_orders['Folio'].iloc[inx]
+
+        items_oc[folio] = purchase_orders_items
+
+        # print(purchase_orders_items)
+    print(items_oc)
+    return items_oc
+
+
+def procesar_os():
+    insumos_col = []
+    costos_col = []
+    purchase_orders = procesar_oc()
     products_cost_df = products_workbook_df[['SKU', 'Costo']]
     # Clean the table
     simplified_service_orders = detailed_service_orders.dropna(
         subset=['Folio'], inplace=False)
-    simplified_service_orders.drop(
-        ['Ultima modificación'], axis=1, inplace=True)
+
     simplified_service_orders.drop(simplified_service_orders.columns[simplified_service_orders.columns.str.contains(
         'unnamed', case=False)], axis=1, inplace=True)
-
-    ordenes_servicio_last_index = get_last_index(detailed_service_orders)
-    simplified_service_orders.index.append(
-        pd.Index([ordenes_servicio_last_index]))
-
-    simplified_service_orders["Insumos"] = pd.Series()
-    simplified_service_orders["Costo total"] = pd.Series()
-
-    simplified_service_orders["Contacto"] = simplified_service_orders.apply(
-        lambda row: get_phone_number(row['Dispositivos Asignados']), axis=1)
-    simplified_service_orders["Garantía?"] = simplified_service_orders.apply(
-        lambda row: get_warranty_status(row['Dispositivos Asignados'], False), axis=1)
-    simplified_service_orders["Dispositivos Asignados"] = simplified_service_orders.apply(
-        lambda row: clean_testimony(row['Dispositivos Asignados']), axis=1)
-    print(simplified_service_orders)
-    # for inx, service_order in simplified_service_orders.iterrows():
-    #     garantia = ""
-    #     (next_inx, next_service_order) = next(
-    #         simplified_service_orders.iterrows())
-    #     print(inx, next_service_order.index)
-    #     dispositivo = service_order['Dispositivos Asignados'][1:]
-    #
-    #     match_corchetes = pattern_corchetes.search(dispositivo)
-
-    #
-
-    #     dispositivo_split = dispositivo.split(' ')
-    #     if match_corchetes:
-    #         garantia = dispositivo_split[-2]
-    #         dispositivo = dispositivo.replace(match_corchetes.group(1), '')
-    #         dispositivo = dispositivo.replace(garantia, '')
-
-    #     else:
-    #         garantia = dispositivo_split[-1]
-    #         dispositivo = dispositivo.replace(garantia, '')
-
-    #     '''Determina si la orden de servicio es garantía y lo añade a un array de elementos
-    #     '''
-    #     if garantia in arr_no:
-    #         garantia_col.append(False)
-    #     elif garantia in arr_yes:
-    #         garantia_col.append(True)
-    #     else:
-    #         garantia_col.append('')
-
-    #     service_order_items = pd.DataFrame(detailed_service_orders.iloc[inx+2:next_inx-2, [
-    #         1, 2, 3, 4, 5, 6, 7, 8]])
-    #     service_order_items.columns = items_cols
-    #     # print(service_order_items)
-    #     items_with_costs = service_order_items.merge(
-    #         products_cost_df, how='left', on='SKU')
-    #     items_cost = inx, list(items_with_costs[[
-    #         'Descripcion', 'Costo']].to_dict('list').values())
-    #     products_cost = items_cost[1]
-    #     resultado = [f'{producto} - {costo}' for producto,
-    #                  costo in zip(products_cost[0], products_cost[1])]
-    #     string = ""
-    #     total_cost = sum(products_cost[1])
-    #     for (i, item) in enumerate(resultado):
-    #         if i > 0:
-    #             string = string + "\n" + item
-    #         else:
-    #             string = string + item
-
-    #     dispositivo = dispositivo.replace('[]', '').strip()
-    #     insumos_col.append(string)
-    #     testimonio_col.append(dispositivo)
-    #     total_cost_col.append(total_cost)
+    simplified_service_orders.drop('Ultima modificación', axis=1, inplace=True)
 
     simplified_service_orders.rename(
-        columns={'Dispositivos Asignados': 'Testimonio'}, inplace=True)
-    # simplified_service_orders.drop(['Ultima modificación'], axis=1, inplace=True)
+        columns={'Dispositivos Asignados': 'Testimonio', 'Entrego': 'Orden de compra'}, inplace=True)
+
+    ordenes_servicio_last_index = get_last_index(detailed_service_orders)
+
+    # Create contact column
+    contact_column = simplified_service_orders.apply(
+        lambda row: get_phone_number(row['Testimonio']), axis=1)
+    simplified_service_orders.insert(3, 'Contacto', contact_column)
+    # Create warranty column
+    warranty = simplified_service_orders.apply(
+        lambda row: get_warranty_status(row['Testimonio'], False), axis=1)
+    simplified_service_orders.insert(5, '¿Garantía?', warranty)
+    # Create testimony column
+    simplified_service_orders['Testimonio'] = simplified_service_orders.apply(
+        lambda row: clean_testimony(row['Testimonio']), axis=1)
+
+    simplified_service_orders['Importe total'] = simplified_service_orders.apply(
+        lambda row: round(float(row['Importe total']), 2), axis=1)
+
+    service_orders_indexs = simplified_service_orders.index.append(
+        pd.Index([ordenes_servicio_last_index]))
+
+    # Process sub table
+    for inx in range(len(service_orders_indexs)-1):
+        service_order_items = pd.DataFrame(detailed_service_orders.iloc[service_orders_indexs[inx]+2:service_orders_indexs[inx+1]-1, [
+            1, 2, 3, 4, 5, 6, 7, 8]])
+        service_order_items.columns = items_cols
+
+        oc = simplified_service_orders['Orden de compra'].iloc[inx]
+
+        items_merge_costs = service_order_items.merge(
+            products_cost_df, how='left', on='SKU')
+
+        products_cost = list(items_merge_costs[[
+            'SKU', 'Cantidad', 'Descripcion', 'Costo']].to_dict('list').values())
+
+        if (type(oc) != float) and (oc in purchase_orders):
+            print(oc)
+            elements = []
+            oc_items_df = purchase_orders[oc]
+            oc_items = list(oc_items_df[[
+                'SKU', 'Cantidad', 'Descripcion', 'Importe']].to_dict('list').values())
+            for i, item in enumerate(products_cost):
+                item += oc_items[i]
+                elements.append(item)
+            products_cost = elements
+            print(products_cost)
+            print('\n')
+
+        products_cost_string = [f'{sku} - {quantity} - {producto} - {round(costo, 2)}' for sku, quantity, producto,
+                                costo in zip(products_cost[0], products_cost[1], products_cost[2], products_cost[3])]
+
+        total_cost = round(sum(products_cost[3]), 2)
+        string = array_to_string(products_cost_string)
+        # print(string)
+        insumos_col.append(string)
+        costos_col.append(total_cost)
+
+    simplified_service_orders.insert(4, "Insumos", insumos_col)
+    simplified_service_orders.insert(5, "Costo total", costos_col)
+
     save_excel(simplified_service_orders, 'only_services', 'Servicios')
 
 
 def proces_sub_table():
     pass
-
-
-def get_last_index(dataframe: pd.DataFrame):
-    """Return the last index of a Pandas Dataframe
-
-    Args:
-        dataframe (pd.DataFrame): Is the dataframe to get the last index
-    """
-    dataframe[len(dataframe) - 1:].index[0]
 
 
 def save_excel(df: pd.DataFrame, book_name: str, sheet_name: str):
