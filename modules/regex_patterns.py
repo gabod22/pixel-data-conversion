194291c3256ac27f15aae03e28ac1b63a2a2c999
gabod22 <briangdr.22@gmail.com>
2024-02-28 18:04:14 -0600

    Change corchetes pattern

--- modules/regex_patterns.py
+++ modules/regex_patterns.py
@@ -1,4 +1,5 @@
 import re
-pattern_celular = re.compile(r'(\d{3}\s?\d{3}\s?\d{4})')
-pattern_corchetes = re.compile(r'\[(.*?)\]')
-pattern_purchase = re.compile(r'(PO[0-9]{5}){1,}(?:,)|(PO[0-9]{5}){1,}')
+
+pattern_celular = re.compile(r"(\d{3}\s?\d{3}\s?\d{4})")
+pattern_corchetes = re.compile(r"(\[.*?\])")
+pattern_purchase = re.compile(r"(PO[0-9]{5}){1,}(?:,)|(PO[0-9]{5}){1,}")
